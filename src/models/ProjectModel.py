d505245db4062606fb717a04a27bf41244997cf4
somaiaahmed <somaia.ahmed03@gmail.com>
2025-07-08 02:48:34 +0300

    Add AssetModel and Asset schema; update data upload route to store assets

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -46,7 +46,7 @@ class ProjectModel(BaseDataModel):
         """
         
         result = await self.collection.insert_one(project.dict(by_alias=True, exclude_unset=True))
-        project._id = result.inserted_id
+        project.id = result.inserted_id
         
         return project
     
