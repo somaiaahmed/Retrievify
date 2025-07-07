bf46846fa9b2e288ef0f6c82760b4538819729c5
somaiaahmed <somaia.ahmed03@gmail.com>
2025-07-08 00:19:27 +0300

    Implement ChunkModel for managing data chunks; update process endpoint to handle chunk creation and deletion

--- src/routes/data.py
+++ src/routes/data.py
@@ -8,6 +8,9 @@ from models import ResponseSignal
 import logging
 from .schemes.data import ProcessRequest
 from models.ProjectModel import ProjectModel
+from models.ChunkModel import ChunkModel
+from models.db_schemes import DataChunk
+
 
 logger = logging.getLogger('uvicorn.error')
 
@@ -72,14 +75,24 @@ async def upload_data(request: Request, project_id: str, file: UploadFile = File
         )
     
 @data_router.post("/process/{project_id}")
-async def process_endpoint(project_id: str, process_request: ProcessRequest, app_settings: Settings = Depends(get_settings)):
+async def process_endpoint(request: Request, project_id: str, process_request: ProcessRequest, app_settings: Settings = Depends(get_settings)):
     """
     Process the uploaded file in the specified project.
     """
     file_id = process_request.file_id
     chunk_size = process_request.chunk_size
     overlap_size = process_request.overlap_size
+    do_reset = process_request.do_reset
     
+    project_model = ProjectModel(
+        db_client=request.app.db_client
+    )
+    
+    project = await project_model.get_project_or_create_one(
+        project_id=project_id
+    )
+    
+        
     process_controller = ProcessController(project_id=project_id)
     
     file_content = process_controller.get_file_content(file_id=file_id)
@@ -99,4 +112,31 @@ async def process_endpoint(project_id: str, process_request: ProcessRequest, app
             }
         )
    
-    return file_chunks
+    file_chunks_records = [
+        DataChunk(
+            chunk_text=chunk.page_content,
+            chunk_metadata=chunk.metadata,
+            chunk_order=i+1,
+            chunk_project_id=project.id,
+        ) for i, chunk in enumerate(file_chunks)
+    ]
+    
+    chunk_model = ChunkModel(
+        db_client=request.app.db_client
+    )
+    
+    if do_reset == 1:
+        _ = await chunk_model.delete_chunks_by_project_id(
+            project_id=project.id
+        )
+        
+    no_records = await chunk_model.insert_many_chunks(
+        chunks=file_chunks_records,
+    )
+    
+    return JSONResponse(
+        content={
+            "signal": ResponseSignal.PROCESSING_SUCCESS.value,
+            "inserted_chunks": no_records
+        }
+    )
