a452ffbd50ae0795cca8afa9349027e98bcbad88
somaiaahmed <somaia.ahmed03@gmail.com>
2025-07-17 03:46:41 +0300

    Add new response signals

--- src/models/enums/ResponseEnum.py
+++ src/models/enums/ResponseEnum.py
@@ -12,4 +12,10 @@ class ResponseSignal(Enum):
     PROCESSING_SUCCESS = "File processed successfully."
     NO_FILES_ERROR = "No files found."
     FILE_ID_ERROR = "NO File found with this id."
+    PROJECT_NOT_FOUND_ERROR = "Project not found."
+    INSERT_INTO_VECTORDB_ERROR = "Error inserting data into the database."
+    INSERT_INTO_VECTORDB_SUCCESS = "Data inserted into the vector database successfully."
+    VECTORDB_COLLECTION_RETRIEVED = "Vector database collection retrieved successfully."
+    VECTORD_SEARCH_SUCCESS = "Vector database search completed successfully."
+    VECTORD_SEARCH_ERORR = "Vector database search failed."
    
