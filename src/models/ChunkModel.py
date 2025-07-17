d5b30d3252e603cee3d4ff8fc6cb91faea04a87c
somaiaahmed <somaia.ahmed03@gmail.com>
2025-07-17 03:45:26 +0300

    Add method to retrieve paginated chunks by project ID

--- src/models/ChunkModel.py
+++ src/models/ChunkModel.py
@@ -98,3 +98,21 @@ class ChunkModel(BaseDataModel):
             })
         
         return result.deleted_count
+    
+    async def get_project_chunks(self, project_id: ObjectId,
+                                 page_no: int = 1, page_size: int = 100):
+        """
+        Get all chunks associated with a specific project.
+        
+        """
+        
+        records = await self.collection.find({
+            "chunk_project_id": project_id
+                }).skip(
+                    (page_no - 1) * page_size
+                ).limit(page_size).to_list(length=None)
+                
+        return [
+            DataChunk(**rec)
+            for rec in records
+        ]
