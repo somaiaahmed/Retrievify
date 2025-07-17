dfd27a3274ed8c10d077ea191e06e1b18debc3ad
somaiaahmed <somaia.ahmed03@gmail.com>
2025-07-17 03:46:16 +0300

    Fix typo

--- src/stores/llm/LLMProviderFactory.py
+++ src/stores/llm/LLMProviderFactory.py
@@ -1,5 +1,5 @@
 from .LLMEnums import LLMEnums
-from .providers import OpenAIProvider, CohereProvider
+from .providers import OpenAIProvider, CoHereProvider
 
 class LLMProviderFactory:
     """
@@ -18,7 +18,7 @@ class LLMProviderFactory:
             )
             
         if provider == LLMEnums.COHERE.value:
-            return CohereProvider(
+            return CoHereProvider(
                 api_key=self.config.COHERE_API_KEY,
                 default_input_max_characters=self.config.GENERATION_DEFAULT_MAX_TOKENS,
                 default_generation_max_output_token=self.config.GENERATION_DEFAULT_MAX_TOKENS,
