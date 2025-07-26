96ac4b9b5dd53c21ad12bcce341b755951ac481a
somaiaahmed <somaia.ahmed03@gmail.com>
2025-07-26 03:16:01 +0300

    Add PGVectorProvider support in VectorDBProviderFactory

--- src/stores/vectordb/VectorDBProviderFactory.py
+++ src/stores/vectordb/VectorDBProviderFactory.py
@@ -1,15 +1,17 @@
-from .providers import QdrantDBProvider
+from .providers import QdrantDBProvider, PGVectorProvider
 from .VectorDBEnums import VectorDBEnums
 from controllers.BaseController import BaseController
+from sqlalchemy.orm import SessionMaker
 
 class VectorDBProviderFactory:
     """
     Factory class to create vector database provider instances.
     """
     
-    def __init__(self, config):
+    def __init__(self, config, db_client: SessionMaker=None):
         self.config = config
         self.base_controller = BaseController()
+        self.db_client = db_client
         
     def create(self, provider: str):
         """
@@ -17,10 +19,20 @@ class VectorDBProviderFactory:
             
         """
         if provider == VectorDBEnums.QDRANT.value:
-            db_path = self.base_controller.get_database_path(db_name=self.config.VECTOR_DB_PATH)
+            qdrant_db_client = self.base_controller.get_database_path(db_name=self.config.VECTOR_DB_PATH)
             return QdrantDBProvider(
-                db_path=db_path,
-                distance_method=self.config.VECTOR_DB_DISTANCE_METHOD
+                db_client=qdrant_db_client,
+                default_vector_size=self.config.EMBEDDING_MODEL_SIZE,
+                distance_method=self.config.VECTOR_DB_DISTANCE_METHOD,
+                index_treshold=self.config.VECTOR_DB_PGVEV_INDEX_THRESHOLD
+            )
+            
+        if provider == VectorDBEnums.PGVECTOR.value:
+            return PGVectorProvider(
+                db_client=self.config.PGVECTOR_DB_CLIENT,
+                default_vector_size=self.config.EMBEDDING_MODEL_SIZE,
+                distance_method=self.config.VECTOR_DB_DISTANCE_METHOD,
+                index_treshold=self.config.VECTOR_DB_PGVEV_INDEX_THRESHOLD
             )
        
         return None  
